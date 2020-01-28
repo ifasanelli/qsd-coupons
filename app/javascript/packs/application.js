29f943b4ac841fc9060cac6464ef89b42508b34e
Wanderler e Luan <par03@campuscode.com.br>
2020-01-28 17:17:55 -0300

    Primeiros models e devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("locawebstyle")
+
+import 'locawebstyle/dist/stylesheets/locastyle.css'
+import 'locawebstyle/dist/javascripts/locastyle.js'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
