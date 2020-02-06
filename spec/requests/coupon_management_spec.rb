57f644471f9f825767362995fd05ae46c78e21df
WanderleiAlexandre <par02@campuscode.com.br>
2020-02-06 17:36:49 -0300

    WIP Implementa consulta de cupons via API

--- spec/requests/coupon_management_spec.rb
+++ spec/requests/coupon_management_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 describe 'Coupons management' do
-    context '- index / list: ' do
-        it 'should render a JSON with all coupons' do
-          promotion = create(:promotion, status: :approved, discount_percentage: 30.0 )
-          cuponA = create(:coupon, code: 'HOSP0001', promotion_id: promotion.id)
-          create(:coupon, code: 'HOSP0002', promotion_id: promotion.id)
-          create(:coupon, code: 'HOSP0003', promotion_id: promotion.id)
-          produto = Product.new(1,'Hospedagem')
+  context '- index / list: ' do
+    it 'should render a JSON with all coupons' do
+      promotion = create(:promotion, status: :approved, discount_percentage: 30.0)
+      cuponA = create(:coupon, code: 'HOSP0001', promotion_id: promotion.id)
+      create(:coupon, code: 'HOSP0002', promotion_id: promotion.id)
+      create(:coupon, code: 'HOSP0003', promotion_id: promotion.id)
 
-          get confer_api_v1_coupon_path(cuponA), params: {coupon: cuponA.code,product: produto.id,price: 1000}
+      get confer_api_v1_coupons_path, params: {coupon: cuponA.code, price: 1000, product: produto.id}
 
-          converted_json = JSON.parse(response.body, symbolize_names: true)
-          expect(response).to have_http_status(:ok)
-          expect(converted_json[0][:code]).to eq('HOSP0001')
-
+      converted_json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:ok)
+      expect(converted_json[:discount]).to eq('300')
     end
   end
 end
