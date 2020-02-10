04ec75f88cbe2739bb8b4f9a13854f5cfbc77bbb
Marco M. Vidal <marcomvidal@gmail.com>
2020-02-10 13:19:38 -0200

    Criação de modelos de dados para facilitar visualizar o resultado final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Promotion.create!([
+  { 
+    description: 'Natal da Loca', prefix: 'NATAL', discount_percentage: 10.0, 
+    max_discount_value: 50, start_date: 1.day.from_now,
+    end_date: 2.days.from_now, max_usage: 10, status: :issued
+  },
+  { 
+    description: 'Páscoa da Loca', prefix: 'PASCOA', discount_percentage: 20.0, 
+    max_discount_value: 75, start_date: Date.current, end_date: 1.day.from_now,
+    max_usage: 10, status: :waiting_for_approval
+  },
+])
+
+Coupon.create!([
+  { code: 'NATAL0001', promotion_id: 1, status: 0 },
+  { code: 'NATAL0002', promotion_id: 1, status: 0 }
+])
+
+User.create!([
+  { email: 'joao@email.com', password: '123456' },
+  { email: 'maria@email.com', password: '123456' }
+])
