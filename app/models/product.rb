d56bd2b4be3ec7561f3ac13d58c2eb207a20fb4c
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-02-11 19:29:20 -0300

    Exclusão de linhas desnecessárias, refatoração de código

--- app/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product
-  attr_accessor :name, :id
-
-  def initialize(id, name)
-    @id = id
-    @nome = name
-  end
-end
--- spec/requests/coupon_confer_spec.rb
+++ spec/requests/coupon_confer_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe 'Coupon confer: ' do
+describe 'Coupon confer' do
   context 'Must be rendered a JSON with the information for a given coupon' do
-    it '(successfully)' do
+    it 'successfully' do
       promotion = create(:promotion, status: :approved,
                                      discount_percentage: 30.0, product_id: 2)
-      coupon = create(:coupon, code: 'HOSP0001', promotion_id: promotion.id,
+      coupon = create(:coupon, code: 'HOSP0001', promotion: promotion,
                                status: :available)
 
       get confer_api_v1_coupons_path, params: { coupon: coupon.code, price: 100,
@@ -16,12 +16,8 @@ describe 'Coupon confer: ' do
       expect(converted_json[:discount]).to eq('30.0')
     end
 
-    it '(non-existent coupon)' do
-      promotion = create(:promotion, status: :approved,
-                                     discount_percentage: 30.0, product_id: 2)
-      create(:coupon, code: 'HOSP0001', promotion_id: promotion.id)
-
-      get confer_api_v1_coupons_path, params: { coupon: 'HOSP0002', price: 100,
+    it 'non-existent coupon' do
+      get confer_api_v1_coupons_path, params: { coupon: 'HOSP9999', price: 100,
                                                 product: 2 }
 
       converted_json = JSON.parse(response.body, symbolize_names: true)
@@ -29,7 +25,7 @@ describe 'Coupon confer: ' do
       expect(converted_json[:error]).to eq('Cupom inexistente')
     end
 
-    it '(invalid coupon)' do
+    it 'a invalid coupon' do
       promotion = create(:promotion, status: :approved,
                                      discount_percentage: 30.0, product_id: 2)
       coupon = create(:coupon, code: 'HOSP0001', promotion_id: promotion.id)
@@ -39,8 +35,22 @@ describe 'Coupon confer: ' do
 
       converted_json = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(422)
-      expect(converted_json[:error]).to eq('Cupom inválido para o produto'\
+      expect(converted_json[:error]).to eq('Cupom inválido para o produto '\
                                             'especificado')
+    end
+
+    it 'a discout bigger than max usage' do
+      promotion = create(:promotion, status: :approved,
+                                     discount_percentage: 50.0, product_id: 2,
+                                     max_discount_value: 5)
+      coupon = create(:coupon, code: 'HOSP0001', promotion_id: promotion.id)
+
+      get confer_api_v1_coupons_path, params: { coupon: coupon.code, price: 100,
+                                                product: 2 }
+
+      converted_json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:ok)
+      expect(converted_json[:discount]).to eq('5.0')
     end
   end
 end
