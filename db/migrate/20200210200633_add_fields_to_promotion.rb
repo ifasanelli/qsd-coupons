8a8fa63307bddbd9be3c25c9faf0a83c542650a9
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-02-19 15:09:22 -0300

    Se aplicado, corrige migrate errada

--- db/migrate/20200210200633_add_fields_to_promotion.rb
+++ db/migrate/20200210200633_add_fields_to_promotion.rb
@@ -1,6 +1,7 @@
 class AddFieldsToPromotion < ActiveRecord::Migration[6.0]
   def change
-   
+    add_column :promotions, :product_id, :integer
+
     add_column :promotions, :product_key, :string
   end
 end
